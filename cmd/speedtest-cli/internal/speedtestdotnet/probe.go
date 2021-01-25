ab8b8ab9f7323f18f06e167194699bf351c586c4
Jon Betti <jonbetti@gmail.com>
2021-01-24 21:16:24 -0500

    Move the updating printer to oututil

--- cmd/speedtest-cli/internal/speedtestdotnet/probe.go
+++ cmd/speedtest-cli/internal/speedtestdotnet/probe.go
@@ -4,100 +4,72 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/mattn/go-isatty"
+	"go.jonnrb.io/speedtest/oututil"
 	"go.jonnrb.io/speedtest/speedtestdotnet"
 	"go.jonnrb.io/speedtest/units"
+	"golang.org/x/sync/errgroup"
 )
-
-var outTTY = false
-
-func init() {
-	outTTY = isatty.IsTerminal(uintptr(os.Stdout.Fd()))
-}
-
-func erasePrevious() {
-	fmt.Print("\033[1A") // ANSI escape sequence for move one line up
-	fmt.Print("\033[K")  // ANSI escape sequence for erase current line
-}
 
 func download(client *speedtestdotnet.Client, server speedtestdotnet.Server) {
 	ctx, cancel := context.WithTimeout(context.Background(), *dlTime)
 	defer cancel()
 
-	printSpeed := func(speed units.BytesPerSecond) {
-		// Default return speed is in bytes.
-		if *fmtBytes {
-			fmt.Printf("Download speed: %v\n", speed)
-		} else {
-			fmt.Printf("Download speed: %v\n", speed.BitsPerSecond())
-		}
-	}
-
-	var stream chan units.BytesPerSecond
-	done := make(chan struct{})
-	if outTTY {
-		stream = make(chan units.BytesPerSecond)
-		go func() {
-			for speed := range stream {
-				erasePrevious()
-				printSpeed(speed)
-			}
-			close(done)
-		}()
-	}
-
-	if outTTY {
-		printSpeed(units.BytesPerSecond(0))
-	}
-	if speed, err := server.ProbeDownloadSpeed(ctx, client, stream); err != nil {
+	stream, finalize := proberPrinter(func(s units.BytesPerSecond) string {
+		return formatSpeed("Download speed", s)
+	})
+	speed, err := server.ProbeDownloadSpeed(ctx, client, stream)
+	if err != nil {
 		log.Fatalf("Error probing download speed: %v", err)
-	} else {
-		if outTTY {
-			<-done
-			erasePrevious()
-		}
-		printSpeed(speed)
+		return
 	}
+	finalize(speed)
 }
 
 func upload(client *speedtestdotnet.Client, server speedtestdotnet.Server) {
 	ctx, cancel := context.WithTimeout(context.Background(), *ulTime)
 	defer cancel()
 
-	printSpeed := func(speed units.BytesPerSecond) {
-		// Default return speed is in bytes.
-		if *fmtBytes {
-			fmt.Printf("Upload speed: %v\n", speed)
-		} else {
-			fmt.Printf("Upload speed: %v\n", speed.BitsPerSecond())
-		}
-	}
-
-	var stream chan units.BytesPerSecond
-	done := make(chan struct{})
-	if outTTY {
-		stream = make(chan units.BytesPerSecond)
-		go func() {
-			for speed := range stream {
-				erasePrevious()
-				printSpeed(speed)
-			}
-			close(done)
-		}()
-	}
-
-	if outTTY {
-		printSpeed(units.BytesPerSecond(0))
-	}
-	if speed, err := server.ProbeUploadSpeed(ctx, client, stream); err != nil {
+	stream, finalize := proberPrinter(func(s units.BytesPerSecond) string {
+		return formatSpeed("Upload speed", s)
+	})
+	speed, err := server.ProbeUploadSpeed(ctx, client, stream)
+	if err != nil {
 		log.Fatalf("Error probing upload speed: %v", err)
-	} else {
-		if outTTY {
-			<-done
-			erasePrevious()
-		}
-		printSpeed(speed)
 	}
+	finalize(speed)
+}
+
+func proberPrinter(format func(units.BytesPerSecond) string) (
+	stream chan units.BytesPerSecond,
+	finalize func(units.BytesPerSecond),
+) {
+	p := oututil.StartPrinting()
+	p.Println(format(units.BytesPerSecond(0)))
+
+	stream = make(chan units.BytesPerSecond)
+	var g errgroup.Group
+	g.Go(func() error {
+		for speed := range stream {
+			p.Println(format(speed))
+		}
+		return nil
+	})
+
+	finalize = func(s units.BytesPerSecond) {
+		g.Wait()
+		p.Finalize(format(s))
+	}
+	return
+}
+
+func formatSpeed(prefix string, s units.BytesPerSecond) string {
+	var i interface{}
+	// Default return speed is in bytes.
+	if *fmtBytes {
+		i = s
+	} else {
+		i = s.BitsPerSecond()
+	}
+	return fmt.Sprintf("%s: %v", prefix, i)
 }
