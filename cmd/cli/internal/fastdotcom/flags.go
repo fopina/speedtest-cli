c1077ca4116aac9b52de091ca92b29c17c55f0ba
Filipe Pina <hzlu1ot0@duck.com>
2026-04-12 08:42:41 +0100

    x

--- cmd/cli/internal/fastdotcom/flags.go
+++ cmd/cli/internal/fastdotcom/flags.go
@@ -18,7 +18,7 @@ var (
 func InitFlags(cmd *cobra.Command) {
 	cmd.Flags().BoolVar(&fmtBytes, "bytes", false, "Display speeds in SI bytes (default is bits)")
 	cmd.Flags().IntVar(&urlCount, "urls", 5, "Number of URLs to use to probe")
-	cmd.Flags().IntVar(&cfgTime, "time.config", 1, "Timeout for getting initial configuration (seconds)")
+	cmd.Flags().IntVar(&cfgTime, "time.config", 3, "Timeout for getting initial configuration (seconds)")
 	cmd.Flags().IntVar(&dlTime, "time.download", 10, "Maximum time to spend in download probe phase (seconds)")
 	cmd.Flags().IntVar(&ulTime, "time.upload", 10, "Maximum time to spend in upload probe phase (seconds)")
 	cmd.Flags().BoolVar(&jsonOut, "json", false, "Output results in JSON format")
